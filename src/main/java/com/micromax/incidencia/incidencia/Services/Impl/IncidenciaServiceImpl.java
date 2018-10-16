e01b7193e2ab4be061484137f25245453e7314d9
Darkona <Javier.Darkona@Gmail.com>
2018-10-16 11:19:15 -0300

    Agregado Logging

--- src/main/java/com/micromax/incidencia/incidencia/Services/Impl/IncidenciaServiceImpl.java
+++ src/main/java/com/micromax/incidencia/incidencia/Services/Impl/IncidenciaServiceImpl.java
@@ -3,6 +3,7 @@ package com.micromax.incidencia.incidencia.Services.Impl;
 import com.micromax.incidencia.incidencia.Services.IncidenciaService;
 import com.micromax.incidencia.incidencia.dao.Incidencia;
 import com.micromax.incidencia.incidencia.repositories.IncidenciaRepository;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -11,6 +12,7 @@ import java.util.List;
 import java.util.Optional;
 
 @Service
+@Slf4j
 public class IncidenciaServiceImpl implements IncidenciaService {
 
     @Autowired
@@ -18,11 +20,13 @@ public class IncidenciaServiceImpl implements IncidenciaService {
 
     @Override
     public List<Incidencia> getIncidencias() {
+        log.info("Buscando todas las incidencias");
         return (ArrayList<Incidencia>) repository.findAll();
     }
 
     @Override
     public Incidencia getIncidencia(long id) {
+        log.info("Buscando Incidencia por id %d", id);
         Optional<Incidencia> i = repository.findById(id);
         return i.orElse(null);
     }
