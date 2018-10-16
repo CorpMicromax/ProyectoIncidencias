919d39c796927fb7bf85bdfc1d8147846727294e
Darkona <Javier.Darkona@Gmail.com>
2018-10-16 00:36:57 -0300

    Segundo commit. Algunos cambios para persistencia. Conexion a Base de datos. Entidades

---
+++ src/main/java/com/micromax/incidencia/incidencia/Status.java
@@ -0,0 +1,6 @@
+package com.micromax.incidencia.incidencia;
+
+public enum Status {
+    NEW, CLOSED, IN_PROGRESS, ASSIGNED
+
+}
--- src/main/java/com/micromax/incidencia/incidencia/dao/Incidencia.java
+++ src/main/java/com/micromax/incidencia/incidencia/dao/Incidencia.java
@@ -1,13 +1,24 @@
 package com.micromax.incidencia.incidencia.dao;
 
+import com.micromax.incidencia.incidencia.Status;
+import com.micromax.incidencia.incidencia.User;
 import lombok.Data;
 
+import javax.persistence.*;
+
 @Data
+@Entity
 public class Incidencia {
+
+    @Id
+    @GeneratedValue(strategy=GenerationType.AUTO)
+    private long id;
 
     private String titulo;
     private String descripcion;
-    private long id;
+    private Status status;
 
+    @OneToOne
+    private User asignado;
 
 }
