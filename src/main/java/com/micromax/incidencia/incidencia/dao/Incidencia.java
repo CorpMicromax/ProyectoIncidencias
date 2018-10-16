429e9380a92f50e943029c1f5f4f0a461c4586b4
Darkona <Javier.Darkona@Gmail.com>
2018-10-15 22:58:38 -0300

    Initial commit

---
+++ src/main/java/com/micromax/incidencia/incidencia/dao/Incidencia.java
@@ -0,0 +1,13 @@
+package com.micromax.incidencia.incidencia.dao;
+
+import lombok.Data;
+
+@Data
+public class Incidencia {
+
+    private String titulo;
+    private String descripcion;
+    private long id;
+
+
+}
