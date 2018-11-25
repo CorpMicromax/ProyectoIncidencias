bd90f721f8bd4a2b8a942f31152ed9c705861036
Darkona <Javier.Darkona@Gmail.com>
2018-11-25 18:20:28 -0300

    Actualizacion de Pom

--- src/main/java/com/micromax/incidencia/config/WebMvcConfig.java
+++ src/main/java/com/micromax/incidencia/config/WebMvcConfig.java
@@ -4,10 +4,6 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.web.servlet.ViewResolver;
-import org.springframework.web.servlet.view.InternalResourceViewResolver;
-import org.springframework.web.servlet.view.jasperreports.JasperReportsMultiFormatView;
-import org.springframework.web.servlet.view.jasperreports.JasperReportsViewResolver;
 
 @Configuration
 @EnableWebSecurity
@@ -18,29 +14,5 @@ public class WebMvcConfig  {
         return new BCryptPasswordEncoder();
     }
 
-    @Bean
-    public JasperReportsViewResolver getJasperReportsViewResolver() {
-
-        JasperReportsViewResolver resolver = new JasperReportsViewResolver();
-        resolver.setPrefix("classpath:jasperreports/");
-        resolver.setSuffix(".jrxml");
-
-        resolver.setReportDataKey("datasource");
-        resolver.setViewNames("*rpt_*");
-        resolver.setViewClass(JasperReportsMultiFormatView.class);
-        resolver.setOrder(0);
-        return resolver;
-    }
-
-    @Bean
-    public ViewResolver htmlViewResolver() {
-
-        InternalResourceViewResolver resolver = new InternalResourceViewResolver();
-
-        resolver.setPrefix("/pages/");
-        resolver.setSuffix(".html");
-        resolver.setOrder(1);
-        return resolver;
-    }
 
 }
